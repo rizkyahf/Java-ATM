f0142161df7848cce33707532c6fef5912a52ab8
LENOVO IP 110 <LENOVO IP 110@DESKTOP-4VPJ7MM>
2018-12-09 18:03:09 +0700

    -

--- src/BankDatabase.java
+++ src/BankDatabase.java
@@ -12,13 +12,16 @@ public class BankDatabase {
    // end myadd
    
    public BankDatabase() {
-      accounts = new Account[4]; // just 2 accounts for testing
+      accounts = new Account[6]; // just 2 accounts for testing
+      // Akun bank
       accounts[0] = new Account(1234, 4321, 1000.0, 1200.0);
       accounts[1] = new Account(8765, 5678, 200.0, 200.0);
       //Akun buat tokopedia
       accounts[2] = new Account(39028765, 1234, 0, 0);
       accounts[3] = new Account(39021234, 1234, 0, 0);
-      
+      // Akun buat ETol
+      accounts[4] = new Account(11111, 1234, 0.0, 0.0);
+      accounts[5] = new Account(22222, 8765, 0.0, 0.0);
       // myadd new
       
       // end add
@@ -72,4 +75,15 @@ public class BankDatabase {
         Account userAccount = getAccount(accountNumber);
         return userAccount != null;
     }
+   
+   boolean cekstatus(int userAccountNumber){
+       Account user = getAccount(userAccountNumber);
+       
+       return user.getStatus();
+   }
+   public void Blokir(int userAccountNumber, boolean Status){
+       Account user = getAccount(userAccountNumber);
+       
+       user.setStatus(Status);
+   }
 } 
