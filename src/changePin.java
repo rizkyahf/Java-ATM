2ade5aaecbe805ba03989748934bb764d090c162
Firna Firdiani <firdianif@gmail.com>
2018-12-20 09:29:31 +0700

    fixing pull in changePin

--- src/changePin.java
+++ src/changePin.java
@@ -21,15 +21,18 @@ public class changePin extends Transaction {
     public void execute() {
         BankDatabase bankDatabase = getBankDatabase();  
         Screen screen = getScreen();
+        Account Akun = bankDatabase.getAccount(getAccountNumber());
         
         screen.displayMessage("\nPlease enter your pin : ");
         int oldPin = keypad.getInput();
         
         
-        if(bankDatabase.authenticateUser(getAccountNumber(), oldPin)) {
-            screen.displayMessage("\nPlease enter your new pin : ");
-            int newPin = keypad.getInput(); // input new pin
-            bankDatabase.ChangePin(getAccountNumber(), newPin);
+        if(bankDatabase.authenticateUser(getAccountNumber())) {
+            if (Akun.validatePIN(oldPin)){
+                screen.displayMessage("\nPlease enter your new pin : ");
+                int newPin = keypad.getInput(); // input new pin
+                bankDatabase.ChangePin(getAccountNumber(), newPin);
+            }
         } 
         else screen.displayMessage("\nPin yang anda masukkan salah....");
     }
